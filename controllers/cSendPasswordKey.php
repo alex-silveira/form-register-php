98cb8234f60687ef7fd1fd72076b517c7ba7e932
alex <alexarauj0015@hotmail.com>
2020-06-14 13:49:12 -0300

    Adicionado modulo de reset de senha.

---
+++ controllers/cSendPasswordKey.php
@@ -0,0 +1,49 @@
+<?php
+    try{
+        include_once '../models/Conexao.class.php';
+        include_once '../models/Usuario.class.php';
+        include_once '../models/Validacao.class.php';
+    }
+    catch(Exception $e)
+    {
+        echo $e;
+    }
+
+    if(!isset($_POST['email']))
+    {
+        $msg = "Operação inválida!";
+        header("Location: ../index.php?msg=$msg");
+        return false;
+    }
+
+    $validacao = new Validacao;
+    $usuario = new Usuario;
+
+    $mail = $usuario->anti_injection($_POST["email"]);
+
+    $usuario->setEmail($mail);
+
+    $gEmail = $usuario->getEmail($mail);
+
+    $key = $usuario->generateKey( $gEmail);
+
+    $msg =  "<h1>Clique no link abaixo para resetar sua senha</h1>".
+            "<a href=https://www.google.com.br/?key=$key>Clique aqui e será redirecionado para página de recuperação!</a>";
+
+
+    try{
+        if($validacao->validarEmail($gEmail)){
+            if($usuario->verificarEmail($gEmail)){
+                $usuario->sendEmail($mail, $msg);
+                $usuario->updateResetPasswordCode($key);
+            }
+        }
+    }
+    catch(Exception $e)
+    {
+        echo $e;
+    }
+
+
+
+
